c6021bcfed7035fc3eb582ec28beadac8d789343
tincho <simonettimarting@gmail.com>
2026-05-31 16:05:36 +0200

    add tournament slider css for centering

--- js/sims/SandboxUI.js
+++ js/sims/SandboxUI.js
@@ -352,7 +352,7 @@ function SandboxUI(config){
 	// Rule: Eliminate/Reproduce how many? (1 to 12)
 	var rule_evolution = _makeLabel("sandbox_rules_2", {x:0, y:100, w:433});
 	var slider_evolution = new Slider({
-		x:0, y:165, width:430,
+		x:0, y:139, width:430,
 		min:1, max:10, step:1,
 		message: "rules/evolution"
 	});
@@ -369,7 +369,7 @@ function SandboxUI(config){
 	// Rule: Noise (0% to 50%)
 	var rule_noise = _makeLabel("sandbox_rules_3", {x:0, y:225, w:433});
 	var slider_noise = new Slider({
-		x:0, y:290, width:430,
+		x:0, y:249, width:430,
 		min:0.00, max:0.50, step:0.01,
 		message: "rules/noise"
 	});
@@ -386,7 +386,7 @@ function SandboxUI(config){
   // Rule: Total tournaments
 var rule_tournaments = _makeLabel("sandbox_rules_4", {x:0, y:350, w:433});
 var slider_tournaments = new Slider({
-    x:0, y:415, width:430,
+    x:0, y:331, width:430,
     min:1, max:200, step:1,
     message: "rules/tournaments"
 });
